b78bd322658116c1d3431c4f41754daf2118cd06
delacruzjross <delacruz.jross@gmail.com>
2023-04-11 20:44:07 +0800

    style: format code in keys.lua

--- keys.lua
+++ keys.lua
@@ -2,63 +2,20 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 return {
-  {
-    key = "F12",
-    action = action.EmitEvent("toggle-opacity"),
-  },
-  {
-    key = "W",
-    mods = "CTRL | SHIFT",
-    action = action.CloseCurrentTab({ confirm = false }),
-  },
-  {
-    key = "Delete",
-    mods = "CTRL | SHIFT",
-    action = action.QuitApplication,
-  },
-  {
-    key = "{",
-    mods = "CTRL | SHIFT",
-    action = action.MoveTabRelative(-1),
-  },
-  {
-    key = "}",
-    mods = "CTRL | SHIFT",
-    action = action.MoveTabRelative(1),
-  },
-  {
-    key = "Space",
-    mods = "LEADER | CTRL",
-    action = action.ActivateCopyMode
-  },
-  {
-    key = "|",
-    mods = "LEADER | SHIFT ",
-    action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "_",
-    mods = "LEADER | SHIFT ",
-    action = action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    key = "h",
-    mods = "LEADER",
-    action = action.ActivatePaneDirection("Left"),
-  },
-  {
-    key = "l",
-    mods = "LEADER",
-    action = action.ActivatePaneDirection("Right"),
-  },
-  {
-    key = "j",
-    mods = "LEADER",
-    action = action.ActivatePaneDirection("Down"),
-  },
-  {
-    key = "k",
-    mods = "LEADER",
-    action = action.ActivatePaneDirection("Up"),
-  },
+  { key = "F12",    action = action.EmitEvent("toggle-opacity") },
+
+  { key = "W",      mods = "CTRL | SHIFT",  action = action.CloseCurrentTab({ confirm = false }) },
+  { key = "Delete", mods = "CTRL | SHIFT",  action = action.QuitApplication },
+  { key = "{",      mods = "CTRL | SHIFT",  action = action.MoveTabRelative(-1) },
+  { key = "}",      mods = "CTRL | SHIFT",  action = action.MoveTabRelative(1) },
+
+  { key = "Space",  mods = "LEADER | CTRL",  action = action.ActivateCopyMode },
+
+  { key = "|",      mods = "LEADER | SHIFT", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "_",      mods = "LEADER | SHIFT", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "h",      mods = "LEADER",         action = action.ActivatePaneDirection("Left") },
+  { key = "l",      mods = "LEADER",         action = action.ActivatePaneDirection("Right") },
+  { key = "j",      mods = "LEADER",         action = action.ActivatePaneDirection("Down") },
+  { key = "k",      mods = "LEADER",         action = action.ActivatePaneDirection("Up") },
+  { key = "p",      mods = "LEADER",         action = action.PaneSelect },
 }
