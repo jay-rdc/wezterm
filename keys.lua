b945a445e674cdb243ad003cb2ef6c70f64b24fb
jay-rdc <delacruz.jross@gmail.com>
2023-04-16 20:01:23 +0800

    style(keys): format code

--- keys.lua
+++ keys.lua
@@ -2,12 +2,12 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 
 return {
-  { key = "F12",    action = action.EmitEvent("toggle-opacity") },
+  { key = "F12",                             action = action.EmitEvent("toggle-opacity") },
 
-  { key = "W",      mods = "CTRL | SHIFT",  action = action.CloseCurrentTab({ confirm = false }) },
-  { key = "Delete", mods = "CTRL | SHIFT",  action = action.QuitApplication },
-  { key = "{",      mods = "CTRL | SHIFT",  action = action.MoveTabRelative(-1) },
-  { key = "}",      mods = "CTRL | SHIFT",  action = action.MoveTabRelative(1) },
+  { key = "W",      mods = "CTRL | SHIFT",   action = action.CloseCurrentTab({ confirm = false }) },
+  { key = "Delete", mods = "CTRL | SHIFT",   action = action.QuitApplication },
+  { key = "{",      mods = "CTRL | SHIFT",   action = action.MoveTabRelative(-1) },
+  { key = "}",      mods = "CTRL | SHIFT",   action = action.MoveTabRelative(1) },
 
   { key = "|",      mods = "LEADER | SHIFT", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
   { key = "_",      mods = "LEADER | SHIFT", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
